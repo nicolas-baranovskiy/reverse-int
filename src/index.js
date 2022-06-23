a7e57e185d7d9ba51eccec563d8ff2966a1b17b6
nicolas-baranovskiy <nicolas.baranovskiy@gmail.com>
2022-06-23 11:36:51 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let newNumber = 0;
+  while (n > 0) {
+    newNumber = (newNumber*10) + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return newNumber;
 }
